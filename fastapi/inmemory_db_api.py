fef68847664cc54c8783928f9b2a279865b7b0b8
mname <uche.chinedu@e4email.net>
2021-11-02 15:59:51 +0100

    update inmemory file

--- fastapi/inmemory_db_api.py
+++ fastapi/inmemory_db_api.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 
-
 app = FastAPI()
 
 #initialize in memory list database
@@ -13,11 +12,9 @@ class City(BaseModel):
     name: str
     timezone: str
 
-
 @app.get('/')
 def index():
     return {'key':'value'}
-
 
 @app.get('/cities')
 def get_cities():
@@ -39,7 +36,6 @@ def get_city(city_id: int):
 def create_city(city: City):
     db.append(city.dict())
     return db[-1]
-
 
 @app.delete('/cities/{city_id}')
 def delete_city(city_id: int):
