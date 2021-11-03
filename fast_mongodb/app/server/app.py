946cf68bf8792a89e56cc96cb84cea022f0d570d
mname <uche.chinedu@e4email.net>
2021-11-03 10:07:53 +0100

    update student route

--- fast_mongodb/app/server/app.py
+++ fast_mongodb/app/server/app.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+
 @app.get("/", tags=["Root"])
 async def read_root():
     return{"message: welcome to the app demo of fastapi with mongodb"}
