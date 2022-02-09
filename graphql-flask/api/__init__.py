a23b386799229d7b0854a00af375bce32cfbca68
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-02-09 11:58:28 +0100

    update database config

--- graphql-flask/api/__init__.py
+++ graphql-flask/api/__init__.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/graphql"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 @app.route('/')
 def hello():
     return 'Graphql API'
