773c3041cd00fa6d5f29e91fbd3c4e27ed07eb42
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-02-09 14:23:43 +0100

    add database files

--- graphql-flask/api/__init__.py
+++ graphql-flask/api/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/graphql"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/graphql?user=graphql&password=graphql"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
